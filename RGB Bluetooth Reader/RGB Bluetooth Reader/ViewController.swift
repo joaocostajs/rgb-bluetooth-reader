97d9bc0bd18f6d1c04974adcf5654a23ef61e966
João Costa <joaocostajjsc@gmail.com>
2020-01-14 23:42:01 +0000

    first

---
+++ RGB Bluetooth Reader/RGB Bluetooth Reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGB Bluetooth Reader
+//
+//  Created by João Costa on 14/01/2020.
+//  Copyright © 2020 João Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
