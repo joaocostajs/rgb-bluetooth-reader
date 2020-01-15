21549a60b6776febac4c487ca71aa1992b50b8bf
João Costa <joaocostajjsc@gmail.com>
2020-01-15 01:24:13 +0000

    teste

--- RGB Bluetooth Reader/RGB Bluetooth Reader/ViewController.swift
+++ RGB Bluetooth Reader/RGB Bluetooth Reader/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Background: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Background.layer.backgroundColor = CGColor(srgbRed: 100.0/255.0, green: 130.0/255.0, blue: 230.0/255.0, alpha: 1)
     }
-
-
+    
+    @IBAction func RGBaction(_ sender: UIButton) {
+        print(sender.title(for: .normal)!)
+        Background.layer.backgroundColor = CGColor(srgbRed: 130.0/255.0, green: 130.0/255.0, blue: 230.0/255.0, alpha: 1)
+    }
+ 
 }
+
 
